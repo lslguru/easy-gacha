7ba93f0d50392eeb3432867484f5e66223e267cb
Zan Lightfoot <d393638e-be6e-4f81-a44d-072e344828c4@lsl.secondlife.com>
2014-01-05 08:54:53 -0500

    Fixing URL

--- v5/global.js
+++ v5/global.js
@@ -12,6 +12,6 @@ define( function() {
 
     // Add bootstrap dependencies
     $('head').append( '<title>Easy Gacha</title>' );
-    $('head').append( '<meta name="viewport" content="width=device-width, initial-scale=1.0">' );
-    $('head').append( '<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">' );
+    $('head').append( '<meta name="viewport" content="width=device-width, initial-scale=1.0" />' );
+    $('head').append( '<link href="' + require.toUrl( './bootstrap/css/bootstrap.min.css' ) + '" rel="stylesheet" />' );
 } );
