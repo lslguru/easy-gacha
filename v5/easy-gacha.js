e1e036aa4d4f782a76870e2fc4941b76922d88d5
Zan Lightfoot <d393638e-be6e-4f81-a44d-072e344828c4@lsl.secondlife.com>
2014-01-05 07:29:01 -0500

    WIP

--- v5/easy-gacha.js
+++ v5/easy-gacha.js
@@ -1,1 +1,7 @@
-alert( 'Test' );
+require( function() {
+    // Switch to HTML5 because SL refuses to serve up content-type text/html
+    document.replaceChild( document.implementation.createDocumentType( 'html' , '' , '' ) , document.doctype );
+    document.documentElement.removeAttribute( 'xmlns' );
+
+    alert( 'Test' );
+} );
